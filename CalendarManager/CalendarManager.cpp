e28e8ac00e656c046c44d74bc6a68066563aef54
beedawn <brandonschmersal@gmail.com>
2025-03-01 20:21:56 -0500

    add tests

--- CalendarManager/CalendarManager.cpp
+++ CalendarManager/CalendarManager.cpp
@@ -15,16 +15,12 @@ std::vector<Calendar> CalendarManager::readCalendars() {
 
 Calendar CalendarManager::newCalendar() {
     Calendar cal;
-
     if (calendars.empty()) {
-        std::cout << "cal manager 0" << std::endl;
         cal.id = 1;
     } else {
         cal.id = calendars.back().id + 1;  // Equivalent to len(cm.Calendars)-1 in Go
     }
-
     calendars.push_back(cal);  // Equivalent to append in Go
-
     return cal;
 }
 
@@ -45,37 +41,31 @@ Calendar CalendarManager::deleteCalendar(const Calendar& c) {
 Resource CalendarManager::newResource() {
     Resource r;
     r.id = static_cast<int>(resources.size())+1;
-
     resources.push_back(r);
     return r;
 }
 
 Resource CalendarManager::deleteResource(Resource r) {
-
     int num = -1;
     for (int i =0; i < resources.size(); i++) {
         if (resources[i].id == r.id) {
             num = i;
         }
     }
-    if (num != -1) {
+    if (num == -1) {
         throw std::out_of_range("resource not found");
     }
-    //what if num is still -1?
     Resource deletedResource = resources[num];
     resources.erase(resources.begin() + num);
-
     return deletedResource;
 }
 using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
 bool CalendarManager::eventOverlap(TimePoint start, TimePoint end, TimePoint start2, TimePoint end2) {
         return (start < end2) && (end < start2);
-
 }
 
 bool CalendarManager::conflictCheck(TimePoint start, TimePoint end, Resource r) {
     int count =0;
-
     for (auto & calendar : calendars) {
         for (auto & event : calendar.events) {
             for (auto & resource : event.resources) {
@@ -90,7 +80,6 @@ bool CalendarManager::conflictCheck(TimePoint start, TimePoint end, Resource r) 
             }
         }
   }
-
     return true;
 }
 
@@ -101,7 +90,6 @@ bool CalendarManager::validateTimes(TimePoint start, TimePoint end) {
     if (end == start) {
         return false;
     }
-
     return true;
 }
 
@@ -110,11 +98,9 @@ bool CalendarManager::validateTimes(TimePoint start, TimePoint end) {
 Event CalendarManager::newEvent(TimePoint start, TimePoint end, Calendar& c, Resource r) {
     if (!validateTimes(start, end)) {
         throw std::runtime_error("end time is before start time");
-
     }
     if (!conflictCheck(start, end, r)) {
         throw std::runtime_error("conflicting event");
-
     }
 
     auto new_event =  Event();
@@ -128,11 +114,9 @@ Event CalendarManager::deleteEvent(Event e, Calendar& c) {
         if (c.events[i].id == e.id) {
             num = i;
         }
-
     }
-    if (num != -1) {
-        throw std::out_of_range("resource not found");
-
+    if (num == -1) {
+        throw std::out_of_range("event not found");
     }
     c.events.erase(c.events.begin() + c.events[num].id);
     return c.events[num];
---
+++ tests/CalendarManager/test_CalendarManager.cpp
@@ -0,0 +1,66 @@
+//
+// Created by Bee Schmersal on 3/1/25.
+//
+
+
+#include <catch2/catch_all.hpp>
+#include "../../CalendarManager/CalendarManager.h"
+#include "../../Calendar/Calendar.h"
+#include "../../Event/Event.h"
+#include "../../Resource/Resource.h"
+
+#include <iostream>
+// TEST_CASE("Example test2") {
+//     REQUIRE(1 + 1 == 2);
+// }
+
+
+TEST_CASE("Calendar Test") {
+    CalendarManager cm;
+    Calendar c = cm.newCalendar();
+    REQUIRE(1 == cm.calendars.size());
+}
+
+TEST_CASE("Calendar Delete Test") {
+    CalendarManager cm;
+    Calendar c = cm.newCalendar();
+    cm.deleteCalendar(c);
+    REQUIRE(cm.calendars.empty());
+}
+
+
+
+
+TEST_CASE("Resource Test") {
+    CalendarManager cm;
+    Resource r = cm.newResource();
+    REQUIRE(1 == cm.resources.size());
+}
+
+TEST_CASE("Resource Delete Test") {
+    CalendarManager cm;
+    const Resource r = cm.newResource();
+    cm.deleteResource(r);
+    REQUIRE(cm.resources.empty());
+}
+
+TEST_CASE("Event Test") {
+    CalendarManager cm;
+    Calendar c = cm.newCalendar();
+    Resource r = cm.newResource();
+    auto now = std::chrono::system_clock::now();
+    auto later = now + std::chrono::seconds(1);
+    Event e = cm.newEvent(now, later, cm.calendars[0], r);
+    REQUIRE(1 == cm.calendars[0].events.size());
+}
+
+TEST_CASE("Event Delete Test") {
+    CalendarManager cm;
+    Calendar c = cm.newCalendar();
+    Resource r = cm.newResource();
+    auto now = std::chrono::system_clock::now();
+    auto later = now + std::chrono::seconds(1);
+    Event e = cm.newEvent(now, later, cm.calendars[0], r);
+    cm.deleteEvent(e, cm.calendars[0]);
+    REQUIRE(cm.calendars[0].events.empty());
+}
